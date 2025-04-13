f910d73b3b2e75b405113a6b8591571421cabaa1
Sarah-BEN-HAMIDA <sarahayoub34@gmail.com>
2025-04-13 13:48:17 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import AdminServicesPage from "./pages/AdminServicesPage";
+import CreateAccountPage from "./pages/CreateAccountPage";
+import EmployeesPage from "./pages/EmployeesPage";
+
+import NotFoundPage from "./pages/NotFoundPage";
+import RapportPage from "./pages/RapportPage";
+import SalesManagementPage from "./pages/SalesManagementPage";
+import ServiceSettingsPage from "./pages/ServiceSettingsPage";
+import SettingsPage from "./pages/SettingsPage";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/admin-services" element={<AdminServicesPage />} />
+        <Route path="/create-account" element={<CreateAccountPage />} />
+        <Route path="/employees" element={<EmployeesPage />} />
+
+        <Route path="/rapport" element={<RapportPage />} />
+        <Route path="/sales-management" element={<SalesManagementPage />} />
+        <Route path="/service-settings" element={<ServiceSettingsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
